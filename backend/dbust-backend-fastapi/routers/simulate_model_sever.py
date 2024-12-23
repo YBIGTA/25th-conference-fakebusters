3be734daa3df9ce3489a99e1fca816aa55872ed5
chaerim <cofla2020@yonsei.ac.kr>
2024-12-23 18:08:21 +0900

    feat: add mmnet model

--- backend/dbust-backend-fastapi/routers/simulate_model_sever.py
+++ backend/dbust-backend-fastapi/routers/simulate_model_sever.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 import random 
+import httpx
 
 router = APIRouter(
     prefix="/api/models",
@@ -11,7 +12,7 @@ router = APIRouter(
 @router.get("/lip/{filekey}")
 async def roi_model(filekey: str):
     '''
-    Simulate the ROI model server
+    Simulate the lipforensic model server
     Input: filekey: str
     Output: response: StreamingResponse which contains the video file and score in the header
     '''
@@ -25,3 +26,32 @@ async def roi_model(filekey: str):
     response.headers['Score'] = f"{score}"
     response.headers['FileKey'] = filekey
     return response
+
+@router.get("/faceroi/{filekey}")
+async def face_roi_model(filekey: str):
+    '''
+    Simulate the Face ROI server
+    Input: filekey: str
+    Output: response: StreamingResponse which contains the video file and score in the header
+    '''
+
+@router.get("/mmnet/{filekey}")
+async def model(filekey: str):
+
+    model_sever_url = "http://165.132.46.87:30310/process_video/"
+    video_file_path = "lip_sample.mp4"
+    # headers = {
+    #     "Content-Type": "multipart/form-data"
+    # }
+
+    try: 
+        with open(video_file_path, "rb") as video_file:
+            async with httpx.AsyncClient() as client:
+                response = await client.post(model_sever_url, files={"file": video_file})
+                response.raise_for_status()
+                return response.json()
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"Error from model server: {e.response.text}")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
