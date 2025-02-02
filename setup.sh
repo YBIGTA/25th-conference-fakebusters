e8a086774da3039a7469f75dc42447677fff897f
1nhye <inhyeyang@yonsei.ac.kr>
2025-02-02 15:31:01 +0000

    feat: add all mode to install all environments

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ echo "Activating Conda environment for: $mode"
 
 if [ "$mode" == "lipforensics" ]; then
     LIP_PATH=$(grep 'lipforensics-path:' $config | cut -d ':' -f 2- | tr -d ' ')
+    echo "LIP_PATH is: $LIP_PATH"
     conda env create -f $LIP_PATH -n lipforensics
     conda activate lipforensics
     pip install mediapipe
@@ -72,7 +73,52 @@ elif [ "$mode" == "frontend" ]; then
     npm un dev
 
 elif [ "$mode" == "all" ]; then
-    echo "will be implemented"
+    echo "Installing all environments..."
+
+    # LipForensics
+    LIP_PATH=$(grep 'lipforensics-path:' $config | awk -F ': ' '{print $2}')
+    echo "Creating lipforensics environment from $LIP_PATH"
+    conda env create -f "$LIP_PATH" -n lipforensics
+    source ~/miniconda3/etc/profile.d/conda.sh
+    conda activate lipforensics
+    pip install mediapipe
+    conda deactivate
+
+    # MMDET
+    MMDET_PATH=$(grep 'mmdet-path:' $config | awk -F ': ' '{print $2}')
+    echo "Creating mmdet environment from $MMDET_PATH"
+    conda env create -f "$MMDET_PATH" -n mmdet
+    source ~/miniconda3/etc/profile.d/conda.sh
+    conda activate mmdet
+    dir_path=$(dirname "$MMDET_PATH")
+    cd "$dir_path/LLaVA"
+    pip install -e .
+    conda deactivate
+
+    # FakeCatcher
+    FAKE_PATH=$(grep 'fakecatcher-path:' $config | awk -F ': ' '{print $2}')
+    echo "Creating fakecatcher environment from $FAKE_PATH"
+    sudo apt-get install -y libgl1-mesa-glx libglx-mesa0 libgl1
+    conda env create -f "$FAKE_PATH" -n fakecatcher
+    source ~/miniconda3/etc/profile.d/conda.sh
+    conda activate fakecatcher
+    conda deactivate
+
+    # Backend
+    BACK_PATH=$(grep 'backend-path:' $config | awk -F ': ' '{print $2}')
+    echo "Creating backend environment from $BACK_PATH"
+    conda env create -f "$BACK_PATH" -n backend
+    source ~/miniconda3/etc/profile.d/conda.sh
+    conda activate backend
+    conda deactivate
+
+    # Frontend
+    FRONT_PATH=$(grep 'frontend-path:' $config | awk -F ': ' '{print $2}')
+    echo "Installing frontend dependencies in $FRONT_PATH"
+    sudo apt install -y npm
+    cd "$FRONT_PATH"
+    npm install
+    npm run dev
 
 else
     echo "Invalid mode: $mode"
